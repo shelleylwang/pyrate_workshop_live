d9c3c47fcc0420126ebb48ad69bf1bb038c85c90
S W <sw@mac.mynetworksettings.com>
2026-05-20 02:32:40 -0400

    gitignore plus additional cluster scripts

--- bdnn_cluster_script.sh
+++ bdnn_cluster_script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=pyrate_workshop_rjmcmc         # Job name
+#SBATCH --job-name=pyrate_workshop_bdnn         # Job name
 #SBATCH --array=1-10                     # Array with 10 independent tasks
 #SBATCH --mem-per-cpu=200MB                # Memory per CPU core (adjust if needed)
 #SBATCH --time=1-00:00:00               # Time limit, e.g., 1 day
 #SBATCH --mail-type=begin               # Send email when job begins
 #SBATCH --mail-type=end                 # Send email when job ends
 #SBATCH --mail-user=sw8569@princeton.edu
-#SBATCH --output=/scratch/gpfs/SIMOES/sw8569/pyrate_workshop/rjmcmc_%j_%A_%a.out
-#SBATCH --error=/scratch/gpfs/SIMOES/sw8569/pyrate_workshop/rmcmc_%j_%A_%a.err
+#SBATCH --output=/scratch/gpfs/SIMOES/sw8569/pyrate_workshop/bdnn_%j_%A_%a.out
+#SBATCH --error=/scratch/gpfs/SIMOES/sw8569/pyrate_workshop/bdnn_%j_%A_%a.err
 #SBATCH --no-requeue                    # Disable requeue
 
 # Change to the directory where the script should run
@@ -21,5 +21,7 @@ module load anaconda3/2024.2
 # Define command to run
 python ../PyRate/PyRate.py input_data/pbdb_data_PyRate.py \
 -qShift Time_bins_ByStages.txt -mG \
--A 4 \
--n 100000 -s 100 -wd rjmcmc -j ${SLURM_ARRAY_TASK_ID} 
+-BDNNmodel 1 \
+-BDNNtimevar predictor_data/environmental_data_standardized.csv \
+-BDNNtimetrait 0 -BDNN_nsim_expected_cv 0 -translate 199 -BDNNnodes 4 2 -BDNNupdate_f 0.3 \
+-n 100000 -s 100 -wd bdnn -j ${SLURM_ARRAY_TASK_ID} 
--- bdsky_cluster_script.sh
+++ bdsky_cluster_script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=pyrate_workshop_rjmcmc         # Job name
+#SBATCH --job-name=pyrate_workshop_bdsky         # Job name
 #SBATCH --array=1-10                     # Array with 10 independent tasks
 #SBATCH --mem-per-cpu=200MB                # Memory per CPU core (adjust if needed)
 #SBATCH --time=1-00:00:00               # Time limit, e.g., 1 day
 #SBATCH --mail-type=begin               # Send email when job begins
 #SBATCH --mail-type=end                 # Send email when job ends
 #SBATCH --mail-user=sw8569@princeton.edu
-#SBATCH --output=/scratch/gpfs/SIMOES/sw8569/pyrate_workshop/rjmcmc_%j_%A_%a.out
-#SBATCH --error=/scratch/gpfs/SIMOES/sw8569/pyrate_workshop/rmcmc_%j_%A_%a.err
+#SBATCH --output=/scratch/gpfs/SIMOES/sw8569/pyrate_workshop/bdsky%j_%A_%a.out
+#SBATCH --error=/scratch/gpfs/SIMOES/sw8569/pyrate_workshop/bdsky_%j_%A_%a.err
 #SBATCH --no-requeue                    # Disable requeue
 
 # Change to the directory where the script should run
@@ -21,5 +21,6 @@ module load anaconda3/2024.2
 # Define command to run
 python ../PyRate/PyRate.py input_data/pbdb_data_PyRate.py \
 -qShift Time_bins_ByStages.txt -mG \
--A 4 \
--n 100000 -s 100 -wd rjmcmc -j ${SLURM_ARRAY_TASK_ID} 
+-fixShift Time_bins_ByStages.txt \
+-A 0 \
+-n 100000 -s 100 -wd bdsky -j ${SLURM_ARRAY_TASK_ID} 
